3747e1850dd8a61313e6372591fd7c415a90e16b
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2019-02-27 23:03:12 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package application;
 
-/**
- *
- * @author chris
- */
+import boardgame.Board;
+
 public class Program {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Board board = new Board(8, 8);
     }
     
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boardgame;
 
-/**
- *
- * @author chris
- */
 public class Position {
     private int row;
     private int column;
