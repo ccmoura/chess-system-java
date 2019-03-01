def20298811b2538caae2aa580821c5b329bab9f
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2019-03-01 01:06:51 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -27,7 +27,7 @@ public class UI {
         if(piece == null){
             System.out.print("-");
         } else{
-            System.out.print("piece");
+            System.out.print(piece);
         }
         System.out.print(" ");
     }
