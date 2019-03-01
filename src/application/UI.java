77f324c9d83817a022651d5f0e60cb64d0d3ac63
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2019-03-01 03:10:32 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,11 @@ public class UI {
         }
     }
 
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("-");
